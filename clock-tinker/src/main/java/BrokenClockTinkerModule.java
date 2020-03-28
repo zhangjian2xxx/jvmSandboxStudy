f35bb8b5c54b670edeb74ec2d0278895bb7ecc0f
zhangjian2xxx <zhangjian2x3.14@aliyun.com>
2020-03-28 13:52:20 +0800

    repairCheckState

--- clock-tinker/src/main/java/BrokenClockTinkerModule.java
+++ clock-tinker/src/main/java/BrokenClockTinkerModule.java
@@ -26,7 +26,7 @@ public class BrokenClockTinkerModule implements Module {
                 .onWatch(new AdviceListener() {
 
                     /**
-                     * 拦截{@code com.taobao.demo.Clock#checkState()}方法，当这个方法抛出异常时将会被
+                     * 拦截{@code Clock#checkState()}方法，当这个方法抛出异常时将会被
                      * AdviceListener#afterThrowing()所拦截
                      */
                     @Override
